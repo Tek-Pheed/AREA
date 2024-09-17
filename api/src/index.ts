f6682cd1ac9b03ab90682fd28b001128a9aafbd8
Alexandre <alexandre.ricard@epitech.eu>
2024-09-17 10:36:28 +0200

    FIX: url in CORS

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader(
         "Access-Control-Allow-Headers",
