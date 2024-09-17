f2e4a4e7d024e7f120f108f9860d198517837ca7
Alexandre <alexandre.ricard@epitech.eu>
2024-09-17 11:36:48 +0200

    ADD: actions cards components

---
+++ area/src/app/components/action_cards_compact/actions_cards_compact.components.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actions-cards-compact',
+  templateUrl: 'actions_cards_compact.components.html',
+  styleUrls: ['actions_cards_compact.components.scss'],
+})
+export class ActionsCardsCompactComponent {
+
+  constructor() {}
+
+}
