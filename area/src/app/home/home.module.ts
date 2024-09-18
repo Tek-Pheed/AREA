34e5c900a338f9d476a86377eb72e19b3093b430
SlyMoz <samy.nasset@epitech.eu>
2024-09-18 15:41:09 +0200

    ADD: profil card component

--- area/src/app/home/home.module.ts
+++ area/src/app/home/home.module.ts
@@ -7,6 +7,7 @@ import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 import { ActionsCardsComponent } from '../components/action_cards/actions_cards.components';
 import { DashboardCardsComponent } from '../components/dashboard_cards/dashboard_cards.components';
+import { ProfilCardComponent } from '../components/profil_card/profil_card.components';
 
 
 @NgModule({
@@ -16,6 +17,6 @@ import { DashboardCardsComponent } from '../components/dashboard_cards/dashboard
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage, ActionsCardsComponent, DashboardCardsComponent]
+  declarations: [HomePage, ActionsCardsComponent, DashboardCardsComponent, ProfilCardComponent]
 })
 export class HomePageModule {}
