4269080f3fed23f9e6e57c6624fb621f23ef08a3
Arnaud AUGAIT <arnaud.augait@epitech.eu>
2024-09-23 17:40:53 +0200

    ADD: Register page

---
+++ area/src/app/register/register.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-Register',
+    templateUrl: 'Register.page.html',
+    styleUrls: ['Register.page.scss'],
+})
+export class RegisterPage {
+
+    constructor() {}
+
+}
