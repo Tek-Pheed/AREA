67300f1af8e16546fc0b1ce5b191afcb18a11d13
LucasLoustalot <lucas.loustalot@epitech.eu>
2024-09-23 12:16:53 +0200

    UPDATE: placeholders

--- area/src/app/profil/profile.page.ts
+++ area/src/app/profil/profile.page.ts
@@ -7,6 +7,12 @@ interface ProfileData {
     ImgSrc: string;
 }
 
+interface APIServices {
+    Name: string;
+    Connected: boolean;
+    ImgSrc: string;
+}
+
 @Component({
     selector: 'app-profil',
     templateUrl: 'profile.page.html',
@@ -21,4 +27,17 @@ export class ProfilePage {
         Email: 'samy.nasset@epitech.eu',
         ImgSrc: 'assets/samy.jpeg',
     };
+
+    servicesData: APIServices[] = [
+        {
+            Name: 'Instagram',
+            Connected: true,
+            ImgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/768px-Instagram_icon.png',
+        },
+        {
+          Name: 'Discord',
+          Connected: false,
+          ImgSrc: 'https://upload.wikimedia.org/wikipedia/fr/thumb/4/4f/Discord_Logo_sans_texte.svg/213px-Discord_Logo_sans_texte.svg.png',
+      },
+    ];
 }
