c2a66fff48879a9909bee39fd634a864c37f37e4
LucasLoustalot <lucas.loustalot@epitech.eu>
2024-09-23 12:04:48 +0200

    UPDATE: profile page

--- area/src/app/profil/profile.page.ts
+++ area/src/app/profil/profile.page.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+interface ProfileData {
+    Name: string;
+    Tag: string;
+    Email: string;
+    ImgSrc: string;
+}
 
 @Component({
-  selector: 'app-profil',
-  templateUrl: 'profile.page.html',
-  styleUrls: ['profile.page.scss'],
-}
-)
+    selector: 'app-profil',
+    templateUrl: 'profile.page.html',
+    styleUrls: ['profile.page.scss'],
+})
 export class ProfilePage {
-  constructor() {}
+    constructor() {}
+
+    data: ProfileData = {
+        Name: 'Samy Nasset',
+        Tag: '@itachi84',
+        Email: 'samy.nasset@epitech.eu',
+        ImgSrc: 'assets/samy.jpeg',
+    };
 }
