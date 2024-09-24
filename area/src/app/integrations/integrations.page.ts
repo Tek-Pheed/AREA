12a92a3cf85c21874298c8e1e2e66ccadf282955
LucasLoustalot <lucas.loustalot@epitech.eu>
2024-09-24 11:30:03 +0200

    UPDATE: action cards

--- area/src/app/integrations/integrations.page.ts
+++ area/src/app/integrations/integrations.page.ts
@@ -5,29 +5,47 @@ interface Integrations {
     iconUrl: String;
 }
 
+interface ActionReaction {
+    apiName: string,
+    description: string,
+}
+
 @Component({
     selector: 'app-integration',
     templateUrl: 'integrations.page.html',
     styleUrls: ['integrations.page.scss'],
 })
 export class IntegrationsPage {
-    constructor() {}
-
     integrations: Integrations[] = [
         {
             name: 'X',
             iconUrl:
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdB-EbJVMxRYtNz1i4dn7jOVSfHq-002oe4w&s',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdB-EbJVMxRYtNz1i4dn7jOVSfHq-002oe4w&s',
         },
         {
             name: 'Twitch',
             iconUrl:
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1vkwHVqqCQ1sF1odRzV_qFB3dGjEaQ_D3zg&s',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1vkwHVqqCQ1sF1odRzV_qFB3dGjEaQ_D3zg&s',
         },
         {
             name: 'Spotify',
             iconUrl:
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/768px-Spotify_logo_without_text.svg.png',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/768px-Spotify_logo_without_text.svg.png',
         },
     ];
+
+    actions: ActionReaction[] = [{apiName: 'X', description: "On tweet"}];
+
+    getIconUrl(apiname: string) {
+        let res = this.integrations.find(({ name }) => name === apiname)?.iconUrl;
+
+        console.warn(res);
+
+        if (res == undefined)
+            return ("assets/favicon.png");
+        return (res);
+    }
+
+    constructor() {
+    }
 }
