81f0b6fa96e06568f3effe512a3a2525ff9b9c66
LucasLoustalot <lucas.loustalot@epitech.eu>
2024-09-25 13:57:24 +0200

    UPDATE: dashboard style

--- area/src/app/dashboard/dashboard.page.ts
+++ area/src/app/dashboard/dashboard.page.ts
@@ -17,8 +17,13 @@ export class DashboardPage {
     data: activeArea[] = [
         {
             name: 'When commit send message on discord channel',
-            actionAPILogoUrl: 'assets/samy.jpeg',
-            reactionAPILogoUrl: 'assets/samy.jpeg',
+            actionAPILogoUrl: 'https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_960_720.png',
+            reactionAPILogoUrl: 'https://static.vecteezy.com/system/resources/previews/006/892/625/non_2x/discord-logo-icon-editorial-free-vector.jpg',
+        },
+        {
+            name: 'When @pixlvlr is live play rush E on spotify',
+            actionAPILogoUrl: 'https://m.media-amazon.com/images/I/21kRx-CJsUL.png',
+            reactionAPILogoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/768px-Spotify_logo_without_text.svg.png',
         },
     ];
 }
