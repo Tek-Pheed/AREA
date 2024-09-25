e5dabe05a52b10c8b4f68fd344a42cac115ea2c5
RaphxelS <scandella@gmx.fr>
2024-09-25 14:08:10 +0200

    ADD: list api

--- api/src/routes/actions/actions.ts
+++ api/src/routes/actions/actions.ts
@@ -1,5 +1,5 @@
 import { Express, Request, Response, Router } from 'express';
-import { getAllActions } from './action.query';
+import { getAllActions, getAllActionsAPI } from './action.query';
 import API from '../../middlewares/api';
 import { auth } from '../../middlewares/auth';
 
@@ -8,6 +8,31 @@ export const actionsRouter = Router();
 actionsRouter.get('/', auth, async (req: Request, res: Response) => {
     res.header('Content-Type', 'application/json');
     const data = await getAllActions();
+    if (data !== null) {
+        /*
+            #swagger.responses[200] = {
+                description: "Some description...",
+                content: {
+                    "application/json": {
+                        schema:{
+                            $ref: "#/components/schemas/actions"
+                        }
+                    }
+                }
+            }
+            #swagger.tags = ['Actions']
+        */
+        res.status(200).json(API(200, false, '', data));
+    } else {
+        res.status(500).json(
+            API(500, true, 'Error when fetching actions', null)
+        );
+    }
+});
+
+actionsRouter.get('/list', auth, async (req: Request, res: Response) => {
+    res.header('Content-Type', 'application/json');
+    const data = await getAllActionsAPI();
     if (data !== null) {
         /*
             #swagger.responses[200] = {
