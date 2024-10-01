9ce7f1d5b99dee848f9cf26da024a2726ee37514
SlyMoz <samy.nasset@epitech.eu>
2024-10-01 15:53:49 +0200

    add: getStreamerStatus for twitch action

--- api/src/routes/twitch/twitch.ts
+++ api/src/routes/twitch/twitch.ts
@@ -14,19 +14,48 @@ const TWITCH_OAUTH_SCOPE = ['user:read:follows', 'user:write:chat'];
 
 export const twitchRouter = Router();
 
-export async function getUserId(token: string): Promise<string> {
-    const resp = await axios.get('https://api.twitch.tv/helix/users/', {
+export async function getUserId(token: string): Promise<any> {
+    const response = await axios.get('https://api.twitch.tv/helix/users/', {
         headers: {
             'Client-ID': TWITCH_CLIENT_ID,
             Authorization: `Bearer ${token}`,
         },
     });
-    return resp.data.data[0].id || null;
+    if (response.data && response.data.data.length > 0) {
+        return response.data.data[0].id;
+    } else {
+        return null;
+    }
+}
+
+export async function getUserLogin(
+    token: string,
+    username: string
+): Promise<any> {
+    const response = await axios.get('https://api.twitch.tv/helix/users', {
+        headers: {
+            'Client-ID': TWITCH_CLIENT_ID,
+            Authorization: `Bearer ${token}`,
+        },
+        params: {
+            login: username,
+        },
+    });
+
+    if (response.data && response.data.data.length > 0) {
+        return response.data.data[0].login;
+    } else {
+        return null;
+    }
 }
 
 export async function getFollowedStreams(token: string): Promise<any> {
     const id = await getUserId(token);
-    if (id === null) return null;
+
+    if (!id) {
+        return null;
+    }
+
     const response = await axios.get(
         `https://api.twitch.tv/helix/streams/followed?user_id=${id}`,
         {
@@ -40,6 +69,48 @@ export async function getFollowedStreams(token: string): Promise<any> {
         return null;
     }
     return response.data || null;
+}
+
+export async function getStreamerStatus(token: string, username: string): Promise<any> {
+    try {
+        const userLogin = await getUserLogin(token, username);
+
+        if (!userLogin) {
+            console.error('Invalid username.');
+            return null;
+        }
+
+        const response = await axios.get(
+            'https://api.twitch.tv/helix/streams',
+            {
+                headers: {
+                    'Client-ID': TWITCH_CLIENT_ID,
+                    Authorization: `Bearer ${token}`,
+                },
+                params: {
+                    user_login: userLogin,
+                },
+            }
+        );
+
+        if (response.data && response.data.data.length > 0) {
+            const streamData = response.data.data[0];
+            return {
+                streamDetails: {
+                    game_name: streamData.game_name,
+                    title: streamData.title,
+                    viewer_count: streamData.viewer_count,
+                },
+            };
+        } else {
+            return {
+                streamDetails: [],
+            };
+        }
+    } catch (error) {
+        console.error(`Error fetching streamer status : ${error}`);
+        return null;
+    }
 }
 
 async function refreshTwitchToken(refreshToken: string): Promise<string> {
