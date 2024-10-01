41dc6447270c47b5a23c1b29d237c5c81a4a16a6
SlyMoz <samy.nasset@epitech.eu>
2024-10-01 16:38:39 +0200

    update: actions functions in actions.ts file

--- api/src/routes/twitch/twitch.ts
+++ api/src/routes/twitch/twitch.ts
@@ -1,6 +1,7 @@
 import { Response, Express, Router } from 'express';
 import { isAuthenticatedTwitch } from '../../middlewares/oauth';
 import { insertTokeninDb } from '../oauth/oauth.query';
+import { getFollowedStreams } from './actions';
 
 const OAuth2Strategy = require('passport-oauth2').Strategy;
 const axios = require('axios');
@@ -45,70 +46,6 @@ export async function getUserLogin(
     if (response.data && response.data.data.length > 0) {
         return response.data.data[0].login;
     } else {
-        return null;
-    }
-}
-
-export async function getFollowedStreams(token: string): Promise<any> {
-    const id = await getUserId(token);
-
-    if (!id) {
-        return null;
-    }
-
-    const response = await axios.get(
-        `https://api.twitch.tv/helix/streams/followed?user_id=${id}`,
-        {
-            headers: {
-                'Client-ID': TWITCH_CLIENT_ID,
-                Authorization: `Bearer ${token}`,
-            },
-        }
-    );
-    if (!response.data) {
-        return null;
-    }
-    return response.data || null;
-}
-
-export async function getStreamerStatus(token: string, username: string): Promise<any> {
-    try {
-        const userLogin = await getUserLogin(token, username);
-
-        if (!userLogin) {
-            console.error('Invalid username.');
-            return null;
-        }
-
-        const response = await axios.get(
-            'https://api.twitch.tv/helix/streams',
-            {
-                headers: {
-                    'Client-ID': TWITCH_CLIENT_ID,
-                    Authorization: `Bearer ${token}`,
-                },
-                params: {
-                    user_login: userLogin,
-                },
-            }
-        );
-
-        if (response.data && response.data.data.length > 0) {
-            const streamData = response.data.data[0];
-            return {
-                streamDetails: {
-                    game_name: streamData.game_name,
-                    title: streamData.title,
-                    viewer_count: streamData.viewer_count,
-                },
-            };
-        } else {
-            return {
-                streamDetails: [],
-            };
-        }
-    } catch (error) {
-        console.error(`Error fetching streamer status : ${error}`);
         return null;
     }
 }
