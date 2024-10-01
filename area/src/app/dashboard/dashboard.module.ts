a009d3eb4668cb310f80c37faafa5727d4484a5c
Arnaud AUGAIT <arnaud.augait@epitech.eu>
2024-10-01 11:32:14 +0200

    ADD:  responsive dashboard page

--- area/src/app/dashboard/dashboard.module.ts
+++ area/src/app/dashboard/dashboard.module.ts
@@ -5,14 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { DashboardPage } from './dashboard.page';
 import { DashboardPageRoutingModule } from './dashboard-routing.module';
 import { DashboardCardsComponent } from '../components/dashboard_cards/dashboard_cards.components';
+import { IntegrationsPageModule } from '../integrations/integrations.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    DashboardPageRoutingModule,
-  ],
-  declarations: [DashboardPage, DashboardCardsComponent]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        DashboardPageRoutingModule,
+        IntegrationsPageModule,
+    ],
+    declarations: [DashboardPage, DashboardCardsComponent],
 })
 export class DashboardPageModule {}
