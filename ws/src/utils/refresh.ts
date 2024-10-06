1c2158a6f4d5001cd910788c943d17ffa600bea3
Alexandre <alexandre.ricard@epitech.eu>
2024-10-06 18:12:20 +0200

    FIX: refresh token function

--- ws/src/utils/refresh.ts
+++ ws/src/utils/refresh.ts
@@ -1,4 +1,5 @@
 import { refreshAccessTokeninDB } from './refresh.query';
+import log from './logger';
 
 const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
@@ -13,29 +14,34 @@ export async function refreshSpotifyToken(
     email: string,
     refreshToken: string
 ): Promise<any> {
-    if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET) {
-        throw new Error('Missing Spotify Client ID or Client Secret');
-    }
-
-    const response = await axios.post(
-        'https://accounts.spotify.com/api/token',
-        new URLSearchParams({
-            grant_type: 'refresh_token',
-            refresh_token: refreshToken,
-            client_id: SPOTIFY_CLIENT_ID,
-            client_secret: SPOTIFY_CLIENT_SECRET,
-        }).toString(),
-        {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
+    try {
+        if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET) {
+            throw new Error('Missing Spotify Client ID or Client Secret');
         }
-    );
-    if (response.data.access_token) {
-        return true;
+
+        const response = await axios.post(
+            `https://accounts.spotify.com/api/token?grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${SPOTIFY_CLIENT_ID}&client_secret=${SPOTIFY_CLIENT_SECRET}`,
+            {},
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }
+        );
+        log.info(response);
+        if (response.data.access_token) {
+            await refreshAccessTokeninDB(
+                email,
+                'spotify',
+                response.data.access_token
+            );
+            return true;
+        }
+        return false;
+    } catch (e) {
+        log.error(e);
+        return false;
     }
-    await refreshAccessTokeninDB(email, 'spotify', response.data.access_token);
-    return false;
 }
 
 export async function refreshDiscordToken(
@@ -90,8 +96,12 @@ export async function refreshTwitchToken(
         }
     );
     if (response.data.access_token) {
-        return false;
+        await refreshAccessTokeninDB(
+            email,
+            'twitch',
+            response.data.access_token
+        );
+        return true;
     }
-    refreshAccessTokeninDB(email, 'twitch', response.data.access_token);
     return false;
 }
