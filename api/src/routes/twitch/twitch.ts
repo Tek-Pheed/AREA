2290813a82923ed75bf859649adecf0315ee77ee
Alexandre <alexandre.ricard@epitech.eu>
2024-10-20 18:32:41 +0200

    ADD: units tests

--- api/src/routes/twitch/twitch.ts
+++ api/src/routes/twitch/twitch.ts
@@ -16,65 +16,6 @@ const TWITCH_OAUTH_SCOPE = [
 
 export const twitchRouter = Router();
 
-export async function getUserId(token: string): Promise<any> {
-    const response = await axios.get('https://api.twitch.tv/helix/users/', {
-        headers: {
-            'Client-ID': TWITCH_CLIENT_ID,
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    if (response.data && response.data.data.length > 0) {
-        return response.data.data[0].id;
-    } else {
-        return null;
-    }
-}
-
-export async function getUserLogin(
-    token: string,
-    username: string
-): Promise<any> {
-    const response = await axios.get('https://api.twitch.tv/helix/users', {
-        headers: {
-            'Client-ID': TWITCH_CLIENT_ID,
-            Authorization: `Bearer ${token}`,
-        },
-        params: {
-            login: username,
-        },
-    });
-
-    if (response.data && response.data.data.length > 0) {
-        return response.data.data[0].login;
-    } else {
-        return null;
-    }
-}
-
-async function refreshTwitchToken(refreshToken: string): Promise<string> {
-    if (!TWITCH_CLIENT_ID || !TWITCH_CLIENT_SECRET) {
-        throw new Error('Missing Twitch Client ID or Client Secret');
-    }
-
-    const response = await axios.post(
-        'https://id.twitch.tv/oauth2/token',
-        new URLSearchParams({
-            grant_type: 'refresh_token',
-            refresh_token: refreshToken,
-            client_id: TWITCH_CLIENT_ID,
-            client_secret: TWITCH_CLIENT_SECRET,
-        }).toString(),
-        {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-        }
-    );
-
-    const newAccessToken = response.data.access_token;
-    return newAccessToken;
-}
-
 twitchRouter.use(
     session({
         secret: process.env.SESSION_SECRET || 'default_secret',
