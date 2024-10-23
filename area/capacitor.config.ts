750484b514380be3759689df760ab8334ca7da1f
Alexandre <alexandre.ricard@epitech.eu>
2024-10-23 17:09:10 +0200

    ADD: deploy to testflight

--- area/capacitor.config.ts
+++ area/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'com.tekpheed.nexus',
+    appId: 'com.epitech.tekpheed.nexus',
     appName: 'Nexus',
     webDir: 'www',
 
