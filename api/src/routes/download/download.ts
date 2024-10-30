87fb34a634c0c9a7ea3eced26ef70b9915405277
LucasLoustalot <lucas.loustalot@epitech.eu>
2024-10-30 18:07:43 +0100

    FIX: apk path

--- api/src/routes/download/download.ts
+++ api/src/routes/download/download.ts
@@ -4,8 +4,8 @@ const fs = require('fs');
 export const downloadRouter = Router();
 
 downloadRouter.get('/', async (req: Request, res: Response) => {
-    if (fs.existsSync('/usr/src/app/src/output/area.apk')) {
-        res.download('/usr/src/app/src/output/area.apk');
+    if (fs.existsSync('/usr/server/src/app/src/output/area.apk')) {
+        res.download('/usr/server/src/app/src/output/area.apk');
     } else {
         res.status(404).send('File not found');
     }
