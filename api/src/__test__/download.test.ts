f645527d832145fcf35694afe915b8adc01b1419
Alexandre <alexandre.ricard@epitech.eu>
2024-11-01 16:15:00 +0100

    RM: db connection in download test

--- api/src/__test__/download.test.ts
+++ api/src/__test__/download.test.ts
@@ -1,49 +1,12 @@
 import supertest from 'supertest';
-import { db, pool } from '../database/db';
 import createTestServer from '../utils/serverTest';
-import log from '../utils/logger';
-require('../../node_modules/mysql2/node_modules/iconv-lite').encodingExists(
-    'foo'
-);
 
 const app = createTestServer();
 
 describe('Download', () => {
-    beforeAll(async () => {
-        return new Promise((resolve, reject) => {
-            pool.getConnection((err, connection) => {
-                if (err) {
-                    log.error(
-                        'Erreur de connexion à la base de données MySQL :',
-                        err
-                    );
-                    reject(err);
-                } else {
-                    log.info('Connexion au pool MySQL réussie.');
-                    connection.release();
-                    resolve(true);
-                }
-            });
-        });
-    });
-
     describe('return 404', () => {
         it('Get log file', async () => {
             await supertest(app).get('/api/download').expect(404);
-        });
-    });
-
-    afterAll(async () => {
-        pool.end((err) => {
-            if (err) {
-                log.error(
-                    'Erreur lors de la fermeture du pool de connexions MySQL :',
-                    err
-                );
-            } else {
-                log.info('Pool de connexions MySQL fermé avec succès.');
-                db.end();
-            }
         });
     });
 });
